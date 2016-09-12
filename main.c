2d8d8b24b7b4e6e37abf6cefdcc1a7f149f5a3f0
av638 <av638@nau.edu>
2016-09-12 16:55:15 -0700

    Added some more comments in Main

--- main.c
+++ main.c
@@ -1,22 +1,27 @@
+// Created by Alejandro Varela
+// Project 1: Images
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct Pixmap // This will serve as my buffer
+
+// This will serve as my buffer
+typedef struct Pixmap
 {
     int width, height, magicNumber;
-
     unsigned char *image;
 
 } Pixmap;
 
-
-
+// Take the file, output file, and desired format
+// Open the file and figure out whether it is in proper format and how to read it
+// Read everything into the buffer and then pass the buffer into a separate function to write
+// to the output file in the desired format
 int ppmConverter(char *inputFile, char *outputFile, int desiredFormat)
 {
     FILE *source;
     int magicNumber;
     char c;
     int width, height, maxColor;
-    int i, j, size, totalItemsRead;
+    int i, j, size, totalItemsRead, pixel;
 
     //Create a buffer for the pixmap image
     Pixmap *buffer = (Pixmap *)malloc(sizeof(Pixmap));
@@ -38,23 +43,15 @@ int ppmConverter(char *inputFile, char *outputFile, int desiredFormat)
     }
 
     c = getc(source);
-
     //skip the comments since they do not matter
     while(c =='#')
     {
-        do
+        c = getc(source);
+        while(c!='\n') //read to the end of the line
         {
             c = getc(source);
         }
-        while(c!='\n'); //read to the end of the line
-        c =getc(source);
     }
-    // double checks to make sure if there is a digit in c
-    if(!isdigit(c))
-    {
-        fprintf(stderr,"can't read header information from PPM format");
-    }
-
     // get it ready to read in the width, height, max color
     ungetc(c, source);
 
@@ -103,7 +100,7 @@ int ppmConverter(char *inputFile, char *outputFile, int desiredFormat)
     }
     // If its in ascii
     else if(magicNumber == 3)
-    { int pixel;
+    {
         for(i=0;i<height;i++)
         {
             for(j=0;j<width;j++)
@@ -128,31 +125,34 @@ int ppmConverter(char *inputFile, char *outputFile, int desiredFormat)
     printf("\nImage has been completely read!");
     pThreeORpSixOut(buffer, outputFile, size, desiredFormat);
     free(buffer);
-    printf("\nImage has been Converted!");
     return 0;
 }
 
+// Takes the buffer, output file, size of image, and the desired format
+// Uses these in order to write the buffer out to the file in either P3 or P6
 void pThreeORpSixOut(Pixmap *buffer, char *outputFileName, int size, int desiredFormat)
 {
+    FILE *destination;
     int i, j, numPix;
     char comment[] = {"#This was converted by Alejandro Varela"};
     //printf("%s", comment);
 
-    FILE *destination = fopen(outputFileName, "w");
+    destination = fopen(outputFileName, "w");
     if (!destination)
     {
-        fprintf(stderr,"\nFile already Exist!!!");
+        fprintf(stderr,"\nCan't open the file for writing");
     }
     else
     {
         fprintf(destination, "P%d\n%s\n%d %d\n%d\n", desiredFormat, comment, buffer->width, buffer->height, 255);
-        //p4-p7
+        // Print out to the outfile in P6 format
         if(desiredFormat == 6)
         {
             numPix = fwrite((void *) buffer->image, 1, (size_t) size, destination);
         }
+        // Print out to the outfile in P3 format
         else if(desiredFormat == 3)
-        {   printf("doing stuff!");
+        {   //printf("doing stuff!");
             for(i=0;i<buffer->height;i++)
             {
                 for(j=0;j<buffer->width;j++)
@@ -161,22 +161,24 @@ void pThreeORpSixOut(Pixmap *buffer, char *outputFileName, int size, int desired
                      fprintf(destination, "%d ",buffer->image[i*buffer->width*3+3*j+1]);
                      fprintf(destination, "%d ",buffer->image[i*buffer->width*3+3*j+2]);
                 }
-
+                 // Add a new line character after each line pixels have been added to the outfile
                  fprintf(destination, "\n");
             }
         }
-    fclose(destination);
     }
+
+    fclose(destination);
     return 0;
 }
 
-
+// Grab the arguments and pass them into the converter
 int main(int argc, char *argv[])
 {
     int ppmFormat = atoi(argv[1]);
 
     printf("Converting starting...");
     ppmConverter(argv[2], argv[3], ppmFormat);
+    printf("\nImage has been Converted!");
 
     return 0;
 }
